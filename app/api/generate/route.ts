0d2472ffd0711daa5a9d0364f44676fedcff2d98
ArmaanTechDeveloper <kournarinder948@gmail.com>
2025-01-10 13:16:40 +0530

    removed caching from the /api/generate api route

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,21 +1,14 @@
 import { generateMnemonic } from "bip39";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
-
+import { unstable_noStore as noStore } from "next/cache";
 
 export function GET() {
     const mnemonic = generateMnemonic();
-  
+    noStore()
     // Create a response and add no-cache headers
     const response = NextResponse.json({
       mnemonic: mnemonic,
     });
-  
-    // Set cache-control headers
-    response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-    response.headers.set("Surrogate-Control", "no-store");
   
     return response;
   }
